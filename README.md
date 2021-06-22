461e8f2ef60b58291a552683d845774e903e7e8b
dyang <danielyang1009@outlook.com>
2021-06-22 20:49:02 +0800

    adding read_shibor

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Basic Quantitative Research Toolbox (BQRT)
+
+This package is built to assist my daily quantitative research, containing research areas of:
+
+- Term structure/fixed income
+- Option pricing
+- ...
+
+## Working with time series models
+
+Lists of python packages maybe used in time series analysis
+
+[Using python to work with time series data](https://github.com/MaxBenChrist/awesome_time_series_in_python)
+
+[pyflux documentations](https://pyflux.readthedocs.io/en/latest/index.html)
+
+
+## Quick Links
+
+[Shibor data download](http://www.shibor.org/shibor/web/DataService.jsp)
---
+++ bqrt/term_structure/read_shibor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+import glob
+from sklearn.linear_model import LinearRegression
+
+
+def read_shibor(path) -> pd.DataFrame:
+    """Batch read Shibor xls files
+
+    Batch read xls files downlaod from Shibor Data Services, with columns of following structure of "date, O/N, 1W, 2W, 1M, 3M, 6M, 9M, 1Y" . For example "myproject\\raw\\shibor"
+
+    [Shibor Data Services](http://www.shibor.org/shibor/web/DataService.jsp) 
+
+    Args:
+        path (str): [file path to folder containing Shibor xls files]
+    """
+    shibor_file_list = glob.glob(path+'\\*.xls')
+    shibor_file_parts = [pd.read_excel(
+        f, parse_dates=['日期']) for f in shibor_file_list]
+    shibor_df = pd.concat(shibor_file_parts, ignore_index=True)
+
+    return shibor_df
+
+
+def rate_interp(date, days):
+    """ term structure linear interpolation
+
+    Return linear interpolation results of interest rate
+    """
+    X = np.array([[30, 90, 180, 270, 360]]).reshape(-1, 1)
+    y = rate_df[rate_df['date'] == date][[
+        '1m', '3m', '6m', '9m', '1y']].values.reshape(-1, 1)
+    reg = LinearRegression().fit(X, y)
+    return reg.predict(np.array([[days]])).item()/100
