c149829db5c7b1439124005716c0e85ed020a75d
dyang <danielyang1009@outlook.com>
2022-06-16 15:10:20 +0800

    Update __init__.py

--- bqrt/util/__init__.py
+++ bqrt/util/__init__.py
@@ -126,6 +126,16 @@ def tsplot(y, lags=None, figsize=(10, 8), style='bmh'):
     return 
 
 
+def jupyter_memory():
+    import sys
+
+    # These are the usual ipython objects, including this one you are creating
+    ipython_vars = ['In', 'Out', 'exit', 'quit', 'get_ipython', 'ipython_vars']
+
+    # Get a sorted list of the objects and their sizes
+    sorted([(x, sys.getsizeof(globals().get(x))) for x in dir() if not x.startswith('_') and x not in sys.modules and x not in ipython_vars], key=lambda x: x[1], reverse=True)
+
+
 def check_dates():
     print(3)
 
