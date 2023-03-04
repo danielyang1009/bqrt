d7a612e000bdf48f68a47aa795eb8c20fb5a20f4
dyang <danielyang1009@outlook.com>
2023-03-04 20:56:12 +0800

    Update time_series.py

--- bqrt/asset_pricing/time_series.py
+++ bqrt/asset_pricing/time_series.py
@@ -8,46 +8,41 @@ import numpy as np
 import pandas as pd
 
 
-def sig_count(data, var):
-
+def count_sig_col(data, var):
     assert isinstance(data, pd.DataFrame), 'data shall be pd.DataFrame'
-
     sig_count = sum([i=='*' for i in data[var].str[-1]])
-
     return sig_count
 
 
-# 单个xvar，显示截距，所有xvar（包括控制）的params和t，并打星
-def singl_xvar_ts(yvar_data, yvar_list, xvar_data, xvar, controls=None, intercept=True, HAC=False, maxlags=None):
+# 输入为singl_xvar_ts与multi_xvar_ts的结果（包含打星的系数和t值）
+def show_coe_only(s):
+    return s.loc[s.index!='']
 
-    assert isinstance(yvar_data, pd.DataFrame) and isinstance(xvar_data, pd.DataFrame), 'yvar_data and xvar_data shall be pd.DataFrame'
-    assert any([isinstance(yvar_list, list), isinstance(yvar_list, pd.Index)]), 'yvar_list shall be a list or pd.Index'
-    assert all([y in yvar_data.columns for y in yvar_list]), 'Y variable(s) not in yvar_data'
-    assert isinstance(xvar, str), 'xvar_list shall be string'
-    assert xvar in xvar_data.columns, 'X variable not in xvar_data'
-    if controls is not None:
-        assert isinstance(controls, list), 'control shall be a list'
-        assert all([x in xvar_data.columns for x in controls]), 'control variable(s) not in xvar_data'
-    assert all(yvar_data.index == xvar_data.index), 'yvar_data and xvar_data index not same'
-    if HAC:
-        assert isinstance(maxlags,int), 'maxlags (int) is needed'
+
+# 输入为singl_xvar_ts与multi_xvar_ts的结果（包含打星的系数和t值）
+def show_sig_only(s):
+    s = show_coe_only(s)
+    return s[s.applymap(lambda x: x[-1]=='*')].fillna('')
+
+
+def singl_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=[], intercept=True, HAC=False, maxlags=None):
 
     from statsmodels.api import OLS, add_constant
 
-    s_cols = ['const']+[xvar]+controls if intercept else [xvar]+controls
-    s_rows = sum([[i,i+'_t'] for i in yvar_data.columns],[])
+    s_cols = ['const']+xvar_list+controls if intercept else xvar_list+controls
+    s_rows = sum([[i,i+'_t'] for i in yvar_data[yvar_list]],[])
     s = pd.DataFrame(index=s_rows, columns=s_cols)
 
-    for yvar in yvar_data.columns:
+    for yvar in yvar_data[yvar_list]:
         Y = yvar_data[yvar].copy()
         if intercept:
-            X = add_constant(xvar_data[[xvar]+controls]).copy()
+            X = add_constant(xvar_data[xvar_list+controls]).copy()
         else:
-            X = xvar_data[[xvar]+controls].copy()
+            X = xvar_data[xvar_list+controls].copy()
 
         if HAC:
             # 用不用use_t没差别
-            reg = OLS(Y, X, missing='drop').fit(cov_type='HAC', cov_kwds={'maxlags':maxlags})
+            reg = OLS(Y, X, missing='drop').fit(cov_type='HAC', cov_kwds={'maxlags':maxlags}, use_t=True)
         else:
             reg = OLS(Y, X, missing='drop').fit(use_t=True)
 
@@ -58,23 +53,38 @@ def singl_xvar_ts(yvar_data, yvar_list, xvar_data, xvar, controls=None, intercep
             s.loc[yvar,col] = f'{param:.4f}' + stars
             s.loc[f'{yvar}_t', col] = f'({tval:.2f})'
 
-    s.index = sum([[i,''] for i in yvar_data.columns],[])
+    s.index = sum([[i,''] for i in yvar_data[yvar_list]],[])
     return s
 
 
-# 多个xvar，仅显示所有xvar的params和t，并打星
-def multi_xvar_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=None, intercept=True, HAC=False, maxlags=None):
+def multi_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=[], intercept=True, HAC=False, maxlags=None):
 
-    assert any([isinstance(xvar_list, list), isinstance(xvar_list, pd.Index)]), 'xvar_list shall be a list or pd.Index'
+    assert isinstance(yvar_data, pd.DataFrame) and isinstance(xvar_data, pd.DataFrame), 'yvar_data and xvar_data shall be pd.DataFrame'
+    assert all(yvar_data.index == xvar_data.index), 'yvar_data and xvar_data index not same'
 
-    s = []
-    for xvar in xvar_list:
-        xvar_reg = singl_xvar_ts(yvar_data, yvar_list, xvar_data, xvar, controls=controls, intercept=intercept, HAC=HAC, maxlags=maxlags)
-        s.append(xvar_reg[xvar].reset_index(drop=True))
+    assert any([isinstance(yvar_list, list), isinstance(yvar_list, pd.Index)]), 'yvar_list shall be a list or pd.Index'
+    assert all([y in yvar_data.columns for y in yvar_list]), 'Y variable(s) not in yvar_data'
 
-    s = pd.DataFrame(s).T
-    s.index = sum([[i,''] for i in yvar_data.columns],[])
+    assert any([isinstance(xvar_list,list), isinstance(xvar_list, pd.Index)]), 'xvars shall be a list or pd.Index'
+    assert all([x in xvar_data.columns for x in xvar_list]), 'X variable(s) not in xvar_data'
 
+    if len(controls) != 0:
+        assert isinstance(controls, list), 'control shall be a list'
+        assert all([x in xvar_data.columns for x in controls]), 'control variable(s) not in xvar_data'
+    if HAC:
+        assert isinstance(maxlags,int), 'maxlags (int) is needed'
+
+    if len(xvar_list) == 1:
+        s = singl_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=controls, intercept=intercept, HAC=HAC, maxlags=maxlags)
+
+    if len(xvar_list) > 1:
+        s = []
+        for xvar in xvar_list:
+            xvar_reg = singl_ts(yvar_data, yvar_list, xvar_data, [xvar], controls=controls, intercept=intercept, HAC=HAC, maxlags=maxlags)
+            s.append(xvar_reg[xvar].reset_index(drop=True))
+        s = pd.DataFrame(s).T
+
+    s.index = sum([[i,''] for i in yvar_data[yvar_list]],[])
     return s
 
 
