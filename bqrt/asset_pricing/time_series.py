3bd11e51990b01868afe0aa374fded4b5a015788
dyang <danielyang1009@outlook.com>
2023-03-06 11:01:09 +0800

    add adj-r2

--- bqrt/asset_pricing/time_series.py
+++ bqrt/asset_pricing/time_series.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 
 def count_sig_col(data, var):
+
     assert isinstance(data, pd.DataFrame), 'data shall be pd.DataFrame'
     sig_count = sum([i=='*' for i in data[var].str[-1]])
     return sig_count
@@ -16,29 +17,43 @@ def count_sig_col(data, var):
 
 # 输入为singl_xvar_ts与multi_xvar_ts的结果（包含打星的系数和t值）
 def show_coe_only(s):
+
     return s.loc[s.index!='']
 
 
 # 输入为singl_xvar_ts与multi_xvar_ts的结果（包含打星的系数和t值）
 def show_sig_only(s):
-    s = show_coe_only(s)
-    return s[s.applymap(lambda x: x[-1]=='*')].fillna('')
+
+    if 'adj-r2' in s.columns:
+        r2 = show_coe_only(s[['adj-r2']])
+        s = show_coe_only(s).drop(columns=['adj-r2'])
+        s = s[s.applymap(lambda x: x[-1]=='*')].fillna('')
+        s = s.merge(r2,left_index=True,right_index=True,how='left')
+    else:
+        s = show_coe_only(s)
+        s = s[s.applymap(lambda x: x[-1]=='*')].fillna('')
+    return s
 
 
 def singl_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=[], intercept=True, HAC=False, maxlags=None):
 
     from statsmodels.api import OLS, add_constant
 
-    s_cols = ['const']+xvar_list+controls if intercept else xvar_list+controls
+    if intercept:
+        xvars = ['const']+xvar_list+controls
+    else:
+        xvars = xvar_list+controls
+
+    s_cols = xvars+['adj-r2']
     s_rows = sum([[i,i+'_t'] for i in yvar_data[yvar_list]],[])
     s = pd.DataFrame(index=s_rows, columns=s_cols)
 
     for yvar in yvar_data[yvar_list]:
-        Y = yvar_data[yvar].copy()
+        Y = yvar_data[yvar]
         if intercept:
-            X = add_constant(xvar_data[xvar_list+controls]).copy()
+            X = add_constant(xvar_data[xvar_list+controls])
         else:
-            X = xvar_data[xvar_list+controls].copy()
+            X = xvar_data[xvar_list+controls]
 
         if HAC:
             # 用不用use_t没差别
@@ -46,15 +61,17 @@ def singl_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=[], intercept=
         else:
             reg = OLS(Y, X, missing='drop').fit(use_t=True)
 
-        for col in s_cols:
+        for col in xvars:
             tval, pval = reg.tvalues[col], reg.pvalues[col]
             param = reg.params[col]
             stars = ''.join(['*' for i in [0.01, 0.05, 0.1] if pval<=i])
             s.loc[yvar,col] = f'{param:.4f}' + stars
             s.loc[f'{yvar}_t', col] = f'({tval:.2f})'
 
+        s.loc[yvar,'adj-r2'] = reg.rsquared_adj
+
     s.index = sum([[i,''] for i in yvar_data[yvar_list]],[])
-    return s
+    return s.fillna('')
 
 
 def multi_ts(yvar_data, yvar_list, xvar_data, xvar_list, controls=[], intercept=True, HAC=False, maxlags=None):
